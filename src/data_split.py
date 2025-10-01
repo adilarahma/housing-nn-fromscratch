d69fcd6874b459b0ec3f07e880521aa802e3449e
adilarahma <adilarahma241@gmail.com>
2025-10-01 15:49:00 +0700

    split dataset into train/test and save CSV

---
+++ src/data_split.py
@@ -0,0 +1,19 @@
+from common import *
+
+df = pd.read_csv('results/housing_clean.csv')
+
+X = df.drop(columns=['price'])
+y = df['price']
+
+# trains-test split 80:20
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# save to results
+X_train.to_csv('results/X_train.csv', index=False)
+X_test.to_csv('results/X_test.csv', index=False)
+y_train.to_csv('results/y_train.csv', index=False)
+y_test.to_csv('results/y_test.csv', index=False)
+
+print("=== Train/test split done ===")
+print("X_train: ", X_train.shape, "\tX_test: ", X_test.shape)
+print("y_train: ", y_train.shape, "\ty_test: ", y_test.shape)
