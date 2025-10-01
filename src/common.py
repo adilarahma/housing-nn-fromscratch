68141d4c5d8bd48162aa70ecb8b5707058127330
adilarahma <adilarahma241@gmail.com>
2025-10-01 15:32:23 +0700

    added common.py for centralized imports

---
+++ src/common.py
@@ -0,0 +1,10 @@
+# all common imports
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
