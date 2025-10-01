5e27ef90c6b2f1594caf4873d217c178a6ffe17c
adilarahma <adilarahma241@gmail.com>
2025-10-01 22:22:32 +0700

    added activation and loss functions

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,40 @@
-# all common imports
+# all common library imports
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+# === activation functions === #
+def relu(x):
+    return np.maximum(0,x)
+
+def relu_derivative(x):
+    return (x>0).astype(float)
+
+def linear(x):
+    return x
+
+def linear_derivative(x):
+    return np.ones_like(x)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    s = sigmoid(x)
+    return s * (1 - s)
+
+def tanh(x):
+    return np.tanh(x)
+
+def tanh_derivative(x):
+    return 1 - np.tanh(x)**2
+
+# === loss functions === #
+def mse(y_true, y_pred):
+    return np.mean((y_true - y_pred)**2)
+
+def mse_derivative(y_true, y_pred):
+    return 2*(y_pred - y_true)/y_true.size
